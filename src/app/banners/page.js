56095e7cbf567a9e295ca70f3465ee6cdd60a6a7
Taner <tanerdokmets@gmail.com>
2025-12-06 14:00:56 +0300

    updated

--- src/app/banners/page.js
+++ src/app/banners/page.js
@@ -1,7 +1,7 @@
 export default function BannerPage() {
   return (
     <div>
-      <h2 className="text-5xl mb-4">Banner</h2>
+      <h2 className="text-5xl mb-4 font-[parkinsans] font-medium">Banner</h2>
       <p className="text-gray-600">Banner content goes here...</p>
     </div>
   );
