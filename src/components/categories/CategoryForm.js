cd1f4e8b96ac4200d6531eaf341f622b60138184
Taner <tanerdokmets@gmail.com>
2025-12-15 18:10:47 +0300

    categoryform updated

--- src/components/categories/CategoryForm.js
+++ src/components/categories/CategoryForm.js
@@ -441,18 +441,23 @@ const CategoryForm = forwardRef(function CategoryForm(
           </div>
 
           {/* 👁️ Görünürlük */}
-          <div className="flex items-center gap-3">
-            <label className="relative inline-flex items-center cursor-pointer">
-              <input
-                type="checkbox"
-                checked={formData.isVisible}
-                onChange={(e) =>
-                  setFormData({ ...formData, isVisible: e.target.checked })
-                }
-                className="sr-only peer"
-              />
-              <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-900/10 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"></div>
-            </label>
+          <div className="rounded-2xl border border-[#DCFFDC] bg-[#F0FFF0] p-4 w-[50%]">
+            <div className="flex items-center justify-between">
+              <span className="text-gray-800 font-medium tracking-wide">
+               Popüler Kategorilerde Göster
+              </span>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={formData.isVisible}
+                  onChange={(e) =>
+                    setFormData({ ...formData, isVisible: e.target.checked })
+                  }
+                  className="sr-only peer"
+                />
+                <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-900/10 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#66CC33]"></div>
+              </label>
+            </div>
           </div>
         </div>
 
