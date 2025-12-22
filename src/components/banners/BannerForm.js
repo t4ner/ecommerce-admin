96ce0f7e716af8705d796e9bdb8e24aa5c9eec56
Taner <tanerdokmets@gmail.com>
2025-12-22 16:41:10 +0300

    BannerForm updated

--- src/components/banners/BannerForm.js
+++ src/components/banners/BannerForm.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, forwardRef, useRef } from "react";
 import { generateSlug } from "@/utils/generateSlug";
-import axios from "axios";
+import apiClient from "@/lib/apiClient";
 
 const BannerForm = forwardRef(function BannerForm(
   { isOpen, onClose, onSubmit, editingBanner },
@@ -64,16 +64,12 @@ const BannerForm = forwardRef(function BannerForm(
       const formDataUpload = new FormData();
       formDataUpload.append("image", file);
 
-      // API'ye gönder
-      const response = await axios.post(
-        "http://localhost:5858/api/upload/single",
-        formDataUpload,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      // API'ye gönder - apiClient kullanarak authentication header'ı otomatik eklenir
+      const response = await apiClient.post("/upload/single", formDataUpload, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
       console.log("📸 Upload Response:", response.data);
 
